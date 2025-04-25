5ffb5f735a834e901768785c996e3478004da2e5
mshevchenkoxa <mshevchenko.xa@gmail.com>
2025-04-25 21:21:39 +0300

    create smart contract

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { WalletContextProvider } from "../context/WalletContext";
+import "./globals.css"
 
 export const metadata:Metadata = {
   title: "Donatverse",
