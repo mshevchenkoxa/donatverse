d46c7d191d420cca72a20d038a86b14e65b56e0a
mshevchenkoxa <mshevchenko.xa@gmail.com>
2025-05-10 14:50:48 +0300

    update contract

--- anchor/donatverse/programs/donatverse/src/lib.rs
+++ anchor/donatverse/programs/donatverse/src/lib.rs
@@ -1,61 +1,76 @@
-// use anchor_lang::prelude::*;
-
-// declare_id!("5wuuViHiEVQvCdBV6cZ7xhvQwB7fsXwb9FLKDAgJXoPK");
-
-// #[program]
-// pub mod donatverse {
-//     use super::*;
-
-//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-//         msg!("Greetings from: {:?}", ctx.program_id);
-//         Ok(())
-//     }
-// }
-
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{program::invoke_signed, system_instruction};
 use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount};
+use solana_program::program::invoke;
 
+
+declare_id!("BJeRvtGt4WrWAu8GGz3FKJd7jbY4gnyqHwLRGxJQjw5J");
 #[program]
-pub mod donation_nft {
+pub mod donatverse {
     use super::*;
 
-    pub fn initialize_donation(
-        ctx: Context<InitializeDonation>,
-        amount: u64,
-    ) -> Result<()> {
+    pub fn initialize_donation(ctx: Context<InitializeDonation>, amount: u64) -> Result<()> {
         let donation = &mut ctx.accounts.donation;
         donation.donor = *ctx.accounts.donor.key;
         donation.recipient = *ctx.accounts.recipient.key;
         donation.amount = amount;
         donation.confirmed = false;
+
+        
+        invoke(
+            &system_instruction::transfer(
+                ctx.accounts.donor.key,
+                ctx.accounts.program_authority.key,
+                amount,
+            ),
+            &[
+                ctx.accounts.donor.to_account_info(),
+                ctx.accounts.program_authority.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+        )?;
+
         Ok(())
     }
 
     pub fn confirm_donation(ctx: Context<ConfirmDonation>) -> Result<()> {
         let donation = &mut ctx.accounts.donation;
 
-        
-        require!(donation.recipient == *ctx.accounts.recipient.key, CustomError::InvalidRecipient);
+        require!(
+            donation.recipient == *ctx.accounts.recipient.key,
+            CustomError::InvalidRecipient
+        );
         require!(!donation.confirmed, CustomError::AlreadyConfirmed);
 
         
         let cpi_accounts = MintTo {
             mint: ctx.accounts.nft_mint.to_account_info(),
             to: ctx.accounts.nft_account.to_account_info(),
-            authority: ctx.accounts.program_authority.to_account_info(),
+            authority: ctx.accounts.program_authority.clone(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info();
-        let signer_seeds: &[&[&[u8]]] = &[&[b"authority", &[ctx.bumps["program_authority"]]]];
-        token::mint_to(CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds), 1)?;
+        let signer_seeds: &[&[&[u8]]] = &[&[b"authority", &[ctx.bumps.program_authority]]];
 
-        /
-        **ctx.accounts.program_authority.try_borrow_mut_lamports()? -= donation.amount;
-        **ctx.accounts.recipient.try_borrow_mut_lamports()? += donation.amount;
+        token::mint_to(
+            CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds),
+            1,
+        )?;
 
         
+        invoke_signed(
+            &system_instruction::transfer(
+                ctx.accounts.program_authority.key,
+                ctx.accounts.recipient.key,
+                donation.amount,
+            ),
+            &[
+                ctx.accounts.program_authority.clone(),
+                ctx.accounts.recipient.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            signer_seeds,
+        )?;
+
         donation.confirmed = true;
 
         Ok(())
@@ -70,6 +85,9 @@ pub struct InitializeDonation<'info> {
     pub donor: Signer<'info>,
     
     pub recipient: UncheckedAccount<'info>,
+    #[account(seeds = [b"authority"], bump)]
+    T
+    pub program_authority: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -77,17 +95,17 @@ pub struct InitializeDonation<'info> {
 pub struct ConfirmDonation<'info> {
     #[account(mut)]
     pub donation: Account<'info, Donation>,
-    
     #[account(mut)]
-    pub recipient: UncheckedAccount<'info>,
+    pub recipient: SystemAccount<'info>,
     #[account(mut)]
     pub nft_mint: Account<'info, Mint>,
     #[account(mut)]
     pub nft_account: Account<'info, TokenAccount>,
-    
     #[account(seeds = [b"authority"], bump)]
-    pub program_authority: UncheckedAccount<'info>,
+    
+    pub program_authority: AccountInfo<'info>,
     pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
